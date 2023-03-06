b24b2222ae89f10bf106f2c88ce1b8bd9290a7df
nabe6414 <123808671+nabe6414@users.noreply.github.com>
2023-03-06 12:46:24 +0000

    create table

--- db/migrate/20230306121416_create_order_datails.rb
+++ db/migrate/20230306121416_create_order_datails.rb
@@ -1,6 +1,11 @@
 class CreateOrderDatails < ActiveRecord::Migration[6.1]
   def change
     create_table :order_datails do |t|
+      t.integer :item_id, null: false
+      t.integer :order_id, null: false
+      t.integer :unit_price, null: false
+      t.integer :amount, null: false
+      t.integer :making_status, null: false, default: 0
 
       t.timestamps
     end
