dcbe82282f81482b3ade3df90953f705bba80aa0
nabe6414 <123808671+nabe6414@users.noreply.github.com>
2023-03-09 11:55:05 +0000

    admin genre item validates

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :genre
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :genre_id, presence: true
+  validates :price, presence: true
+  validates :is_active, inclusion: {in: [true, false]}
+  validates :image, presence: true
 end
