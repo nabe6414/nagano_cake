5d3143fe24bbf610abfa85baddcf81ee5321621f
nabe6414 <123808671+nabe6414@users.noreply.github.com>
2023-03-10 12:29:21 +0000

    customer sign_in sign_up error修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  devise_for :customers
+  devise_for :customers, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
