15699ebc2f700de350bc39369e2acccd687192a1
nabe6414 <123808671+nabe6414@users.noreply.github.com>
2023-03-12 11:57:22 +0000

    create admin top view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get '/' => 'homes#top'
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
