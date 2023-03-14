67a9829b151f7bcf59fc3e26300083d496340e94
nabe6414 <123808671+nabe6414@users.noreply.github.com>
2023-03-14 11:41:55 +0000

    update admin order show view

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_datails, dependent: :destroy
+
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { not_payment: 0, payment: 1, manufacture: 2, shipping_preparation: 3, shipped: 4 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
     get '/' => 'homes#top'
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
   end
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
