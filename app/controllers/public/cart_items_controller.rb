63333f2da6d6b36d94ebf980fde07451f38d67cb
nabe6414 <123808671+nabe6414@users.noreply.github.com>
2023-03-22 13:28:33 +0000

    cart機能作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,11 +3,22 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
+    @cart_item = CartItem.new(params[:id])
   end
 
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.save
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    redirect_to cart_items_path
+  end
+
+  def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to cart_items_path
   end
 
   private
