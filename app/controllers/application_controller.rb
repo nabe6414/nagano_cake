e94558170bf3f5e65a0b0b4441b3313b7e064279
nabe6414 <123808671+nabe6414@users.noreply.github.com>
2023-03-22 12:01:15 +0000

    権限の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_admin!, if: :admin_url
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
+
+  def admin_url
+    request.fullpath.include?("/admin")
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana,
