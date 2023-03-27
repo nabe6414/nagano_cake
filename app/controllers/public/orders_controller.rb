b90083423f6e642095df8885dde2ac835438c7ab
nabe6414 <123808671+nabe6414@users.noreply.github.com>
2023-03-27 14:23:37 +0000

    注文情報入力 お届け先選択 作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,6 +6,19 @@ class Public::OrdersController < ApplicationController
   def confirm
     @cart_items = current_customer.cart_items.all
     @order = Order.new(order_params)
+    if params[:order][:address_select] == '0'
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:address_select] == '1'
+      @order.postal_code = Address.find(params[:order][:address_id]).postal_code
+      @order.address = Address.find(params[:order][:address_id]).address
+      @order.name = Address.find(params[:order][:address_id]).name
+    elsif params[:order][:address_select] == '2'
+      @order = Order.new(order_address_params)
+    else
+      render :new
+    end
   end
 
   def create
@@ -24,5 +37,9 @@ class Public::OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:payment_method)
+  end
+
+  def order_address_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 end
