bb4bb7dc51ec3372a2d1bdbad6334005eb04042c
nabe6414 <123808671+nabe6414@users.noreply.github.com>
2023-03-28 12:51:17 +0000

    小計,合計表示修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,6 +6,7 @@ class Public::OrdersController < ApplicationController
   def confirm
     @cart_items = current_customer.cart_items.all
     @order = Order.new(order_params)
+    @total = 0
     if params[:order][:address_select] == '0'
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
@@ -15,16 +16,20 @@ class Public::OrdersController < ApplicationController
       @order.address = Address.find(params[:order][:address_id]).address
       @order.name = Address.find(params[:order][:address_id]).name
     elsif params[:order][:address_select] == '2'
-      @order = Order.new(order_address_params)
+      @order = Order.new(order_params)
     else
       render :new
     end
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to complete_orders_path
   end
 
   def index
+    @orders = current_customer.orders.all
   end
 
   def show
@@ -36,10 +41,6 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method)
-  end
-
-  def order_address_params
     params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 end
